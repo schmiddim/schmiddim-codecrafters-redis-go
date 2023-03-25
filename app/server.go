4a835ec14ed7df9f4934dffbf7905b49b3d927b1
Michael Schmitt <schmiddim@gmx.at>
2023-03-25 23:03:23 +0100

    Refactoring

--- app/server.go
+++ app/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+)
+
+const (
+	ConnHost = "0.0.0.0"
+	ConnPort = "6379"
+	ConnType = "tcp"
 )
 
 func main() {
@@ -13,43 +19,55 @@ func main() {
 
 	//Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen(ConnType, ConnHost+":"+ConnPort)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+	// Close the listener when the application closes.
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		}
+	}(l)
+	fmt.Println("Listening on " + ConnHost + ":" + ConnPort)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+		}
+		go handleRequest(conn)
+
 	}
-	var messages []string
-	scanner := bufio.NewScanner(conn)
 
-	//scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		message := scanner.Text()
-		messages = append(messages, message)
-		fmt.Println(scanner.Text())
-		if message == "ping" {
-			fmt.Printf("cmd  %s received\n", message)
+}
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	brr := string(buf)
+	fmt.Println("Message received:", brr)
+
+	for _, element := range strings.Split(brr, "\n") {
+		if strings.TrimRight(strings.ToLower(element), "\r") == "ping" {
 			_, err = conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				return
-			}
-		}
-		if message == "" {
-			break
 		}
 	}
-	//_, err = conn.Write([]byte("+PONG\r\n"))
-	//if err != nil {
-	//	return
-	//}
+	// Send a response back to person contacting us.
+	if err != nil {
+		return
+	}
+	// Close the connection when you're done with it.
 	err = conn.Close()
 	if err != nil {
 		return
 	}
-
 }
