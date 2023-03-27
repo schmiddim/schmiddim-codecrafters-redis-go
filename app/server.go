32dd7a5711ebeda930ccbc1bef964ba285cdfc90
Michael Schmitt <schmiddim@gmx.at>
2023-03-27 19:48:24 +0200

    Step 4 done

--- app/server.go
+++ app/server.go
@@ -14,11 +14,6 @@ const (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	//Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen(ConnType, ConnHost+":"+ConnPort)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
@@ -38,36 +33,29 @@ func main() {
 			fmt.Println("Error accepting: ", err.Error())
 		}
 		go handleRequest(conn)
-
 	}
-
 }
 func handleRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
-
-	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-	}
-
-	brr := string(buf)
-	fmt.Println("Message received:", brr)
-
-	for _, element := range strings.Split(brr, "\n") {
-		if strings.TrimRight(strings.ToLower(element), "\r") == "ping" {
-			_, err = conn.Write([]byte("+PONG\r\n"))
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("Error closing connection")
 		}
-	}
-	// Send a response back to person contacting us.
-	if err != nil {
-		return
-	}
-	// Close the connection when you're done with it.
-	err = conn.Close()
-	if err != nil {
-		return
+	}(conn)
+	for {
+		buf := make([]byte, 1024)
+		// Read the incoming connection into the buffer.
+		_, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading:", err.Error())
+		}
+		for _, element := range strings.Split(string(buf), "\n") {
+			if strings.TrimRight(strings.ToLower(element), "\r") == "ping" {
+				_, err = conn.Write([]byte("+PONG\r\n"))
+			}
+		}
+		if err != nil {
+			return
+		}
 	}
 }
